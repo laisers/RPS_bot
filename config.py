b9342e0510c7bc01b34c6ab43830ab529dc253a1
laisers <74301420+laisers@users.noreply.github.com>
2021-07-25 17:08:56 +0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+# User  configuration
+
+USER_STARTBALANCE = 0 # Balance starting value
+USER_STARTWINS = 0 # Wins starting value
+
+# Bot configuration
+
+
+BOT_TOKEN = ''
+QIWI_TOKEN = ''
+
+#others
+
+ADMINS = [781776587] # ID bot administrators, no more than three
